264d58e79e77e312c508c773c57c0202305047f4
Jacob Wisniewski <wisniewskij26@gmail.com>
2017-12-24 12:28:46 -0600

    add ts linting

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
-import "./styles/index.scss";
-import App from "./App";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import './styles/index.scss';
+import App from './App';
 
 const render = (Component: any) => {
   ReactDOM.render(
     <AppContainer>
       <Component />
     </AppContainer>,
-    document.getElementById("root")
+    document.getElementById('root')
   );
 };
 
 render(App);
 
 if ((module as any).hot) {
-  (module as any).hot.accept("./App", () => {
-    const NextApp = require("./App").default;
+  (module as any).hot.accept('./App', () => {
+    const NextApp = require('./App').default;
     render(NextApp);
   });
 }
