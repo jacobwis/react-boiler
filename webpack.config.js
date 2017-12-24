549f81f8e3748f7b9df1de59322fcc8d2f1d4e4b
Jacob Wisniewski <wisniewskij26@gmail.com>
2017-12-24 12:30:33 -0600

    add stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
-const path = require("path");
-const webpack = require("webpack");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ["react-hot-loader/patch", "./src/index.tsx"],
+  entry: ['react-hot-loader/patch', './src/index.tsx'],
   output: {
-    filename: "build.js",
-    path: path.resolve(__dirname, "build")
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'build')
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
+  devtool: 'eval-source-map',
   module: {
     loaders: [
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
+        loader: 'awesome-typescript-loader'
       },
       {
         test: /\.scss$/,
         use: [
-          "style-loader",
-          { loader: "css-loader", options: { importLoaders: 1 } },
-          "postcss-loader",
-          "sass-loader"
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+          'sass-loader'
         ]
       }
     ]
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: "index.html"
+      template: 'index.html'
     }),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
@@ -38,7 +39,7 @@ module.exports = {
   devServer: {
     compress: true,
     historyApiFallback: true,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     hot: true,
     port: 3000
   }
